7982b929c2ac83fc464f84bf70013708f07172c9
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-05-29 10:27:06 -0700

    Implement solution for Task 3 - Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Integers division with debug
+"""
+
+
+def safe_print_division(a, b):
+    """
+    Divides two integers and prints the result
+    """
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
+
+
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
