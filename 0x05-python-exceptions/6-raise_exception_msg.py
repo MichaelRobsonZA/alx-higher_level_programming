63ffe79a6e553ad8dae932b7325b0d4d0aeb3747
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-05-29 10:27:23 -0700

    Add solution for Task 6 - Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Raise a message exception
+"""
+
+
+def raise_exception_msg(message=""):
+    """
+    Raises a name exception with a message
+    """
+    raise NameError(message)
+
+
+if __name__ == "__main__":
+    try:
+        raise_exception_msg("C is fun")
+    except NameError as ne:
+        print(ne)
