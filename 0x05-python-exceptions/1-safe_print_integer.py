9a4cbf06ceeb50cdc804085694d16c49af70df66
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-05-29 10:15:48 -0700

    solutions for task 0 & 1 & README.md and the test files

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Safe printing of an integers list
+"""
+
+
+def safe_print_integer(value):
+    """
+    Prints an integer with "{:d}".format()
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        return False
+
+
+if __name__ == "__main__":
+    value = 89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "School"
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
