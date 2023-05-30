873264790ba93941ac246e23b8ec09554a8549d2
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-05-30 03:46:22 -0700

    Implement my_print() method to print the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Computes the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def my_print(self):
+        """
+        Prints the square with the character '#'.
+        """
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__size):
+            print("#" * self.__size)
+
