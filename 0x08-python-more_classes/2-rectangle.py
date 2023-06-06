236f1924d04ee593031051194a9079640d56a7a3
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-06-06 03:19:09 -0700

    added check for if width or height is 0, the perimeter method will return 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module 2-rectangle
+Defines a Rectangle class
+"""
+
 
 class Rectangle:
     """
@@ -58,6 +63,6 @@ class Rectangle:
         """
         Calculates the perimeter of the rectangle
         """
-        if self.__width == 0 or self.__height == 0:
-            return 0
+	 if self.__width == 0 or self.__height == 0:
+        return 0
         return 2 * (self.__width + self.__height)
