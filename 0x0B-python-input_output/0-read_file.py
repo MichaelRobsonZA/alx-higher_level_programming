e84667a6421d578804bd5c026a10c31d6d48ea91
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-06-13 14:47:24 -0700

    Requirement fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This module contains a function that reads a text file (UTF8) and p
+This module contains a function that reads a text file and prints it to stdout.
+"""
+
 def read_file(filename=""):
-    """
-
-
+"""
     Read and print the contents of a text file.
-
+    
     Args:
         filename (str): The name of the file to read.
 
