1bac2f2b326390e1bdf544a5f19d7eb431272d5d
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-06-13 10:24:53 -0700

    fix for 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    """Reads a text file and prints its content to stdout.
-
-    Args:
-        filename (str): The name of the file to be read.
-
-    Returns:
-        None
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
+    with open(filename, encoding='utf-8') as file:
         print(file.read(), end='')
-
-
-read_file()
