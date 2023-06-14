7807da9ecd3a2df9e40f1c8c4e80b29f671578b9
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-06-14 04:15:43 -0700

    corrections

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -13,16 +13,4 @@ class MyList(list):
         """
         Prints the list in sorted order.
         """
-        print(sorted(self))
-
-
-if __name__ == "__main__":
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
+        print(sorted(self)))
