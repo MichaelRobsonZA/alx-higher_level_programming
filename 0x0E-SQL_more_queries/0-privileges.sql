9d14723cc562891798f564274b654123ba580b4d
MichaelRobsonZA <122970847+MichaelRobsonZA@users.noreply.github.com>
2023-07-19 14:17:34 +0200

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- List privileges of user_0d_1
+-- List privileges of user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges of user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
