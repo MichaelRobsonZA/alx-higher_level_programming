7b8b65ebef4ad54d07186b8dd2cb997ec850e0d0
MichaelRobsonZA <122970847+MichaelRobsonZA@users.noreply.github.com>
2023-08-19 11:15:26 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-"""Lists all states from DB hbtn_0e_0_usa with a name starting with
-N (upper N) using SQLAlchemy"""
-
+"""Lists all City objects from the database hbtn_0e_101_usa
+using SQLAlchemy.
+"""
 
 import sys
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
+    # Set up the connection to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, database),
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a Session instance
     session = Session()
 
-    states = session.query(State).filter
-    (State.name.like('%N%')).order_by(State.id).all()
+    # Query the database for cities and their corresponding states
+    cities_and_states = session.query(City, State).\
+                        filter(City.state_id == State.id).order_by(City.id).all()
 
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    # Print the results
+    for city, state in cities_and_states:
+        print("{}: ({}) {}".format(city.name, state.id, state.name))
 
+    # Close the session
     session.close()
