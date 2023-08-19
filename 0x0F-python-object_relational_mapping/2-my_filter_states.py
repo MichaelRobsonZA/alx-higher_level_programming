9d36214ea39fc5263b39c5332fc7726583f045f7
MichaelRobsonZA <122970847+MichaelRobsonZA@users.noreply.github.com>
2023-08-19 11:29:11 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
-"""Lists all values in the states table of 
-hbtn_0e_0_usa where name matches the argument"""
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
-    database_name = sys.argv[3]
-    search_name = sys.argv[4]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    db = MySQLdb.connect
-    (host="localhost", port=3306, user=username, passwd=password, db=database_name)
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=password,
+                         db=database,
+                         port=3306)
+    
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (search_name,))
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
     cursor.close()
     db.close()
