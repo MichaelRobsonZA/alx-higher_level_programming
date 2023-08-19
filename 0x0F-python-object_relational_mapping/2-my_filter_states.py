7bc3cdafe0d4ca821db1f2c3f0b5aec20e1f0174
MichaelRobsonZA <122970847+MichaelRobsonZA@users.noreply.github.com>
2023-08-19 11:22:43 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
-"""Lists all City objects from the database hbtn_0e_101_usa
-using SQLAlchemy.
-"""
+"""Lists all values in the states table of 
+hbtn_0e_0_usa where name matches the argument"""
 
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+import MySQLdb
 
 if __name__ == "__main__":
-    # Set up the connection to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    search_name = sys.argv[4]
 
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session instance
-    session = Session()
-
-    # Query the database for cities and their corresponding states
-    cities_and_states = session.query(City, State).\
-                        filter(City.state_id == State.id).order_by(City.id).all()
-
-    # Print the results
-    for city, state in cities_and_states:
-        print("{}: ({}) {}".format(city.name, state.id, state.name))
-
-    # Close the session
-    session.close()
+    db = MySQLdb.connect
+    (host="localhost", port=3306, user=username, passwd=password, db=database_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (search_name,))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
