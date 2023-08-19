a4314c338268b44f7bb0a46c7176ec5698cb24ea
MichaelRobsonZA <122970847+MichaelRobsonZA@users.noreply.github.com>
2023-08-19 10:29:03 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""
-Takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
-"""
+"""Lists all values in the states table of hbtn_0e_0_usa where name matches the argument"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database_name = sys.argv[3]
     search_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=db_name)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-                (search_name,))
-    states = cur.fetchall()
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (search_name,))
+    states = cursor.fetchall()
     for state in states:
         print(state)
-    cur.close()
+    cursor.close()
     db.close()
