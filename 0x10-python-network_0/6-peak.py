527c73ba969d565163bfaf85e13540639a466b8d
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-08-31 12:16:39 -0700

    Implement basic peak finding algorithm with logarithmic complexity

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
+        return None
+
+    peak = list_of_integers[0]
+
+    for num in list_of_integers:
+        if num > peak:
+            peak = num
+
+    return peak
