8746515045c6774e8f2e968720391a6992fe4f6c
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 15:58:19 -0700

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Fetches and displays the status from a URL using urllib."""
 import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
+
 with urllib.request.urlopen(url) as response:
-    content = response.read()
-    print("Body response:")
-    print(f"    - type: {type(content)}")
-    print(f"    - content: {content}")
-    print(f"    - utf8 content: {content.decode('utf-8')}")
+    body = response.read()
+
+print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", body.decode('utf-8'))
