ff68f8f131f23b84c2f6e5f53390ddd4a73771f5
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 15:28:55 -0700

    Task 6: Send POST request with email and display response.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email address, sends a POST request to
-the passed URL with the email as a parameter, and finally displays the
-body of the response using the requests and sys packages.
+Takes a URL and an email address, sends a POST request with the email as a parameter,
+and displays the response content using the requests package.
 """
 
-import requests
 import sys
+import requests
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = {"email": email}
-response = requests.post(url, data=data)
-
-print("Your email is:", email)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {"email": email}
+    response = requests.post(url, data=payload)
+    print("Your email is:", email)
