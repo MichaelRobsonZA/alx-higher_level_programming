c2381bcffd585d162a74fdc21f8b1a2daa242145
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 15:28:48 -0700

    Task 5: Fetch and display X-Request-Id from URL headers.

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request to the URL, and displays the
-value of the variable X-Request-Id in the response header using the
-requests and sys packages.
+Takes a URL as input, sends a request, and displays the X-Request-Id header value
+using the requests package.
 """
 
-import requests
 import sys
+import requests
 
-url = sys.argv[1]
-
-response = requests.get(url)
-x_request_id = response.headers.get("X-Request-Id")
-
-print(x_request_id)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    x_request_id = response.headers.get("X-Request-Id")
+    print(x_request_id)
