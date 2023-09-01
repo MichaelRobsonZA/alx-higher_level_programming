9d50be6b57c38d97558317d3a3c8e7f4e1ff65b7
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 15:28:39 -0700

    Task 4: Fetch and display the status from URL using requests.

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-Script that fetches https://alx-intranet.hbtn.io/status using the requests
-package.
+Fetches https://alx-intranet.hbtn.io/status using the requests package
+and displays information about the response content.
 """
 
 import requests
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    content_type = type(response.text).__name__
+    utf8_content = response.text
 
-response = requests.get(url)
-content = response.text
-
-print("Body response:")
-print("    - type:", type(content))
-print("    - content:", content)
+    print("Body response:")
+    print(f"    - type: {content_type}")
+    print(f"    - content: {utf8_content}")
