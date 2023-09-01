4ad42331f5fae745b242889de980525ca9a8d5bd
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 16:02:50 -0700

    Updated Task 7 code for compliance with requirements and improved error handling.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,7 +7,6 @@ url = sys.argv[1]
 try:
     response = requests.get(url)
     response.raise_for_status()
-    content = response.text
-    print(content)
+    print(response.text)
 except requests.exceptions.HTTPError as e:
-    print("Error code:", e.response.status_code)
+    print(f"Error code: {e.response.status_code}")
