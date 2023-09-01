0d829574c5438848112b12e3d97e4fcc28e32169
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 07:38:35 -0700

    Add script for Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+letter = sys.argv[1] if len(sys.argv) > 1 else ""
+url = "http://0.0.0.0:5000/search_user"
+data = {"q": letter}
+
+try:
+    response = requests.post(url, data=data)
+    response_data = response.json()
+
+    if "id" in response_data and "name" in response_data:
+        print("[{}] {}".format(response_data["id"], response_data["name"]))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
