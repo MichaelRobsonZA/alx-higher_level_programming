2a053667b9ed6158ca0998fdd64fbd1bcf20f24a
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 15:28:24 -0700

    Task 2: Send POST request with email and display response.

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response
-(decoded in utf-8). It uses the packages urllib and sys.
+Takes a URL and an email address, sends a POST request with the email as a parameter,
+and displays the response content using the requests package.
 """
 
-import urllib.request
-import urllib.parse
 import sys
+import requests
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = urllib.parse.urlencode({"email": email}).encode("utf-8")
-req = urllib.request.Request(url, data)
-
-with urllib.request.urlopen(req) as response:
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {"email": email}
+    response = requests.post(url, data=payload)
     print("Your email is:", email)
