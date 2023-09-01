23a71595e8c7a0db1a90562fe2dbc7decef393de
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 07:38:09 -0700

    Add script for Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+url = sys.argv[1]
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    print(response.text)
+except requests.exceptions.HTTPError as e:
+    print(f"Error code: {e.response.status_code}")
