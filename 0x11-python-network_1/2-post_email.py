70cc0310bc2463fcd040cc4e6de9ced2cbb6c45d
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 16:02:28 -0700

    Modified Task 2 code to meet requirements and enhance simplicity.

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,8 +7,7 @@ url = sys.argv[1]
 email = sys.argv[2]
 
 data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data=data, method='POST')
+req = urllib.request.Request(url, data)
 
 with urllib.request.urlopen(req) as response:
-    content = response.read().decode('utf-8')
-    print(f"Your email is: {content}")
+    print("Your email is:", response.read().decode('utf-8'))
