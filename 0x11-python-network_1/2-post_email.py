fe2f01f711e6b8d75df25394407976d4916bb135
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-01 07:29:25 -0700

    Add script for Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+req = urllib.request.Request(url, data)
+
+with urllib.request.urlopen(req) as response:
+    print("Your email is:", email)
