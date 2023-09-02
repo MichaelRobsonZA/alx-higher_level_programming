c180bc8076b3e8b323c71e79719a8c8708d98cf8
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-02 03:50:36 -0700

    corrections

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,6 +7,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} <URL>".format(sys.argv[0]))
+        sys.exit(1)
+
     url = sys.argv[1]
 
     response = requests.get(url)
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
+
     url, email = sys.argv[1], sys.argv[2]
     data = {'email': email}
     response = requests.post(url, data=data)
