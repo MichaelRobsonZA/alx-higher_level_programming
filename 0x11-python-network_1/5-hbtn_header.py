fb418072df117086ba5813ce51c58c30e6fe9a85
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-02 03:22:25 -0700

    corrections 5&6 secondtry

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     response = requests.get(url)
     if "X-Request-Id" in response.headers:
         x_request_id = response.headers["X-Request-Id"]
-        print(x_request_id)
+        print(x_request_id.strip())
     else:
         print("X-Request-Id header not found in the response.")
