3e48d0bcd7608afd2bbbc0b3f1a6c938c65f7713
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-09-02 04:17:09 -0700

    correction 4

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
+"""
+Sends a POST request to http://0.0.0.0:5000/search_user with a letter as a parameter.
+"""
+
 import requests
 import sys
 
-letter = sys.argv[1] if len(sys.argv) > 1 else ""
-url = "http://0.0.0.0:5000/search_user"
-data = {"q": letter}
-try:
-    response = requests.post(url, data=data)
-    response_data = response.json()
-    if "id" in response_data and "name" in response_data:
-        print("[{}] {}".format(response_data["id"], response_data["name"]))
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
     else:
+        q = sys.argv[1]
+
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': q}
+
+    try:
+        response = requests.post(url, data=data)
+        json_response = response.json()
+
+        if json_response:
+            print("[{}] {}".format(json_response.get('id'), json_response.get('name')))
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
+
+    except Exception as e:
         print("No result")
-except ValueError:
-    print("Not a valid JSON")
